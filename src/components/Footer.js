ec7a533a2db0dccb1df99ad5774d3dce688def6a
minhm23 <minhmai2304@gmail.com>
2024-07-25 03:09:36 -0700

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="text-center text-muted">
-			<p>&copy; 2024 Minh Mai. All rights reserved.</p>
-        </footer>
-    )
-}
