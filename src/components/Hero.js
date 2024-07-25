0d95123cc1b03d20aab62bccc2721b79b32aee32
minhm23 <minhmai2304@gmail.com>
2024-07-25 02:49:25 -0700

    half-way checkpoint

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,23 @@
+// import Button from 'react-bootstrap/Button';
+import '../styles/hero.css';
+
 export default function Hero() {
+    const openResume = () => {
+        window.open("https://drive.google.com/file/d/1Lh5ctVd1ABZjl8EZBb_NB4yd2tp_UWY4/view?usp=sharing", "_blank");
+    };
+
     return (
-        <section id="#hero" className="hero">
-            <h1>HERO</h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <section id="hero" className="hero fade-in">
+            <h1>Hi, I am <strong>Minh Mai</strong><span className="dot">.</span></h1>
+            <p>Frontend Developer based in Seattle, WA</p>
+            <div className="button-container">
+                <button className="resume-button" onClick={openResume}>
+                    <span>View Resume</span>
+                </button>
+                <button className="blog-button">
+                    <span>View Blog</span>
+                </button>
+            </div>
         </section>
     )
 }
